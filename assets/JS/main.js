ee40f53ce7c0dbbfc4a9ab6f636c38953130a5b6
Olivier Frugier <sedatif2@MacBook-Pro-de-Olivier.local>
2021-04-25 23:44:05 +0700

    vanilla3

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,11 +1,33 @@
 const $ = document;
-// $.addEventListener("click", () => {
-//   console.log("Click !!!");
-// });
+
 $.addEventListener("DOMContentLoaded", () => {
+  const modal = $.querySelector(".modal");
+  const body = $.querySelector("body");
+  $.querySelector("#button").addEventListener("click", () => {
+    // Display modal
+    modal.classList.remove("hidden");
+    body.classList.add("disable");
+  });
+  $.querySelector(".to-website").addEventListener("click", () => {
+    // Hide modal
+    modal.classList.add("hidden");
+    // Enable scroll
+    body.classList.remove("disable");
+  });
   console.log("Page chargée");
 
-  $.querySelector("#button").addEventListener("click", () => {
-    $.querySelector(".masked").classList.add("show");
-  });
+  //   contactForm = $.querySelector("#contactForm").addEventListener;
+  //   contactForm.addEventListener("submit", (event) => {
+  //     // la fonction preventDefault() annule le comportement par défaut du formulaire (le rafraîchissement de la page)
+  //     event.preventDefault();
+
+  //     const data = {
+  //       firstName: $.querySelector("#firstname").value,
+  //       lastName: $.querySelector("#lastname").value,
+  //       email: $.querySelector("#email").value,
+  //       subject: $.querySelector("#subject").value,
+  //       message: $.querySelector("#message").value,
+  //     };
+
+  //   });
 });
