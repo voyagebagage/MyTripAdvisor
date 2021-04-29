86522be06c4a1a325b029d8d2f324162ac0f4ad4
Olivier Frugier <sedatif2@MacBook-Pro-de-Olivier.local>
2021-04-29 18:32:50 +0700

    netlify ready

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -14,26 +14,45 @@ $.addEventListener("DOMContentLoaded", () => {
     // Enable scroll
     body.classList.remove("disable");
   });
-  console.log("Page chargée");
+  // console.log("Page chargée");
 
   contactForm = $.querySelector("#contactForm");
+  const button = $.querySelector("#send-button");
+  const h4 = $.querySelector("h4");
+
   contactForm.addEventListener("submit", async (event) => {
     // la fonction preventDefault() annule le comportement par défaut du formulaire (le rafraîchissement de la page)
     event.preventDefault();
+    //for one send at the time
+    button.setAttribute("disabled", "disabled");
+    //change the color at the click but not working
+    button.classList.add("send-button-disabled");
     console.log("Page chargée");
-    const data = {
+
+    const form = {
       firstName: $.querySelector("#firstname").value,
       lastName: $.querySelector("#lastname").value,
       email: $.querySelector("#email").value,
       subject: $.querySelector("#subject").value,
       message: $.querySelector("#message").value,
     };
-    const response = await axios.get("http://localhost:3000/", data);
+    console.log(form);
+
+    const response = await axios.get(
+      "https://tripadvisor-backend-form.herokuapp.com/",
+      form
+    );
     console.log(response);
     if (response.status !== 200) {
-      alert("n'as pas pu etre envoyé");
+      alert("n'as pas pu etre envoywrong request, try again");
+      button.removeAttribute("disabled");
+      button.classList.remove("send-button-disabled");
     } else {
       alert("sent ok :)");
+      button.removeAttribute("disabled");
+      button.classList.remove("send-button-disabled");
+      h4.classList.remove("hidden");
+      contactForm.reset();
     }
   });
 });
